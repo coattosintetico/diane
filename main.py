9e335114f003d65811a8f6b5d1a6d5cca3c25654
Manuel Sánchez <manuel.sanchez2@klarna.com>
2024-10-13 00:20:06 +0200

    remove file after

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import datetime
 import subprocess
+import os
+
+now = datetime.datetime.now().strftime(r"%Y%m%d%H%M%S")
+FILENAME = f"./data/{now}.m4a"
 
 def record():
-    subprocess.run(["termux-microphone-record", "-f", "./data/audio.m4a"], check=True, stdout=subprocess.DEVNULL)
+    subprocess.run(["termux-microphone-record", "-f", FILENAME], check=True, stdout=subprocess.DEVNULL)
 
 def stop():
     subprocess.run(["termux-microphone-record", "-q"], check=True, stdout=subprocess.DEVNULL)
+    os.remove(FILENAME)  # Remove the file after stopping the recording
 
 if __name__ == "__main__":
     record()
