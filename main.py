e75b8b04142df66d9b2af8ef5d969481fc484076
Manuel Sánchez <manuel.sanchez2@klarna.com>
2024-10-13 01:11:30 +0200

    Change formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
-import subprocess
 import os
+import subprocess
 from pathlib import Path
 
 REPOSITORY_PATH = Path("/data/data/com.termux/files/home/diane")
@@ -8,16 +8,19 @@ DATA_FOLDER_PATH = REPOSITORY_PATH / "data"
 
 FILENAME = DATA_FOLDER_PATH / f"{datetime.datetime.now().strftime(r'%Y%m%d%H%M%S')}.m4a"
 
+
 def record():
     subprocess.run(["termux-microphone-record", "-f", FILENAME], check=True, stdout=subprocess.DEVNULL)
+
 
 def stop():
     subprocess.run(["termux-microphone-record", "-q"], check=True, stdout=subprocess.DEVNULL)
     os.remove(FILENAME)  # Remove the file after stopping the recording
 
+
 if __name__ == "__main__":
-    print("\n\tHEY... I'M DIANE\n")
+    print("\nHEY... I'M DIANE\n")
     record()
-    input("(👉👂 listening...)")
+    input("\n\t(👉👂 listening...)\n")
     stop()
-    print("\n\tthx Cooper, noted\n")
+    print("\nthx Cooper, noted\n")
