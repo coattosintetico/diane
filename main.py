90f0d1b6c72beedbdb4b00e5d36889a83f2ed235
Manuel Sánchez <manuel.sanchez2@klarna.com>
2024-11-09 22:03:31 +0100

    update

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def main():
         print(f"│ category:    {expense.category.value}")
         print("└───")
         print()
-        confirmation = input("is that right or correct description? (Y/n/r)")
+        confirmation = input("is that right or correct description? (Y/[d]escription/[r]etry)")
         if confirmation.lower() == "r":
             print("sorry, retrying...")
             print()
